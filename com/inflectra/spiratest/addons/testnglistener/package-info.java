e49038f597dfec78317f51c4e3da4ba170b2b5c5
fkrivsky <fkrivsky@CZPBCLPJVN3PQ2.eu.global.ad>
2020-08-03 17:27:20 +0200

    Removing old folder structure.

--- com/inflectra/spiratest/addons/testnglistener/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains the core functionality for the SpiraTest listener for TestNG
- *
- * @since 1.5.1
- */
-package com.inflectra.spiratest.addons.testnglistener;
--- com/inflectra/spiratest/addons/testnglistener/samples/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides examples on how to use TestNG 5.7 with SpiraTest v1.2+
- *
- * @since 4.0
- */
-package com.inflectra.spiratest.addons.testnglistener.samples;
